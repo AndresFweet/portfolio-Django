5610f6bc2b7f6ea444f0d43f4205b52a2af7e4fe
STAY-DEV <andresjh10@hotmail.com>
2024-12-17 21:16:10 -0500

    static media

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -134,7 +134,7 @@ if not DEBUG:
 
 MEDIA_ROOT = BASE_DIR / 'media'
 
-MEDIA_URL = '/public/'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
